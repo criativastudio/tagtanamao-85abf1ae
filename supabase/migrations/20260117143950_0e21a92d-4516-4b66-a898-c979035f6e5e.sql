4cfae743227d1144b530296b0718401a0e75feeb
Criativa Studio <criativastudiobr@gmail.com>
2026-02-24 00:47:46 -0400

    Update 20260117143950_0e21a92d-4516-4b66-a898-c979035f6e5e.sql

--- supabase/migrations/20260117143950_0e21a92d-4516-4b66-a898-c979035f6e5e.sql
+++ supabase/migrations/20260117143950_0e21a92d-4516-4b66-a898-c979035f6e5e.sql
@@ -55,6 +55,8 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER SET search_path = public;
 
+DROP TRIGGER IF EXISTS on_auth_user_created ON auth.users;
+
 CREATE TRIGGER on_auth_user_created
   AFTER INSERT ON auth.users
   FOR EACH ROW EXECUTE FUNCTION public.handle_new_user();
